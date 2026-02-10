bdba21370b122f097b4b8972d79fb84885e219c9
nono-zarazua <nono.zarazua@gmail.com>
2026-02-10 19:35:41 +0100

    Fixed cache in kroki

--- MGraph.jl
+++ MGraph.jl
@@ -1046,41 +1046,44 @@ kroki(graph2dot(gnew(type="werner"),type="digraph"),type="graphviz")
 
 """
 
-function kroki(text="A --> B";filename=nothing,type="ditaa",ext="png",cache=true,name=nothing)
+
+function kroki(text="A --> B"; filename=nothing, type="ditaa", ext="png", cache=true, name=nothing)
     if !isnothing(filename)
         if !isfile(filename)
             error("Error: file " * filename * " does not exist!")
         else
             text = read(filename, String)
         end
-
     end
 
     data = transcode(ZlibCompressor, codeunits(text))
     b64string = base64encode(data)
-    url = replace(b64string, "+" => "-", "/" => "_", "=" => "")
-    url = "https://kroki.io/$type/$ext/$url"
+    safe_b64 = replace(b64string, "+" => "-", "/" => "_", "=" => "")
+    url = "https://kroki.io/$type/$ext/$safe_b64"
 
-    if cache
-        if !isdir("img")
-            mkdir("img")
-        end
-        if name !== nothing
-            filename = "$(name).$(ext)"
-            imgname = joinpath("img", filename)
-        else
-            chk = string(crc32c(url), base=16)
-            filename = "$(chk).$(ext)"
-            imgname = joinpath("img",filename)
-        end
-        if !isfile(imgname)
-            print("Downloading $(name === nothing ? "..." : name)...\n")
-            Downloads.download(url,imgname)
-        end
-        return imgname
-    else
-        return(url)
+    if !isdir("img")
+        mkdir("img")
     end
+
+    if isnothing(name)
+        chk = string(crc32c(url), base=16)
+        out_filename = "$(chk).$(ext)"
+    else
+        out_filename = "$(name).$(ext)"
+    end
+    imgname = joinpath("img", out_filename)
+
+    if !isfile(imgname) || !cache
+        print("Downloading $(out_filename)...\n")
+        try
+            Downloads.download(url, imgname)
+        catch e
+            rm(imgname, force=true) 
+            error("Failed to download image. Check syntax or internet.")
+        end
+    end
+
+    return imgname
 end
 
 """#'
@@ -1904,233 +1907,229 @@ const HELP_TEXT="""
 
 function main(argv)
     println(argv)  
-    if "--help" in argv
+    if isempty(argv) || "--help" in argv
         println(HELP_TEXT)
         return
     end
+    
     validcmd = ["--gnew","--snha","--create-data"]
-    if length(argv) >= 1
-        if !(argv[1] in validcmd)
-            error("The argument used is not a valid argument from $validcmd")
-        elseif argv[1] == "--gnew"
-            validtypes = ["werner","barabasi"]
-            if !(argv[2] in validtypes)
-                error("The second argument for --gnew must be a valid type of graph in $validtypes")
-            else
-                type = argv[2]
-                if length(argv) == 2
-                    g = gnew(type=argv[2]) 
-                    show(stdout, "text/plain", g)
-                    println()
-                else
-                    args = Dict{Symbol,Any}()
-                    valid_args = ["--nodes", "--edges", "-v", "--mode", "-o"]
-                    v = false
-                    o = false
-                    output = "gnew-output.csv"
-                    i = 3
-                    while i <= length(argv)
-                        val = nothing
-                        if !(argv[i] in valid_args)
-                            error("Option $(argv[i]) not valid for --gnew. See valid options: $valid_args.\n")
-                        end
+    if !(argv[1] in validcmd)
+        error("The argument used is not a valid argument from $validcmd")
+    elseif argv[1] == "--gnew"
+        validtypes = ["werner","barabasi"]
+        if length(argv) < 2 || !(argv[2] in validtypes)
+            error("The second argument for --gnew must be a valid type of graph in $validtypes")
+        end
 
-                        if (i+1) <= length(argv) && !(argv[i+1] in valid_args)
-                            val = argv[i+1]
-                        end
-
-                        if argv[i] == "--nodes"
-                            valint = tryparse(Int, val)
-                            if isnothing(valint)
-                                error("The value for --nodes must be an integer. You provided: $val.")
-                            end
-                            args[:nodes] = valint
-                        elseif argv[i] == "--edges"
-                            valint = tryparse(Int, val)
-                            if isnothing(valint)
-                                error("The value for --edges must be an integer. You provided: $val.")
-                            end
-                            args[:edges] = valint
-                        elseif argv[i] == "-v" 
-                            v = true
-                            i += 1
-                            continue
-                        elseif argv[i] == "--mode"
-                            if !(val in ["directed", "undirected"])
-                                error("The value for --mode should be 'directed' or 'undirected'. You provided: $val.")
-                            end
-                            args[:mode] = val
-                        elseif argv[i] == "-o"
-                            o = true
-                            if isnothing(val)
-                                i += 1
-                                continue
-                            else
-                                output = val
-                            end
-                        end
-                        i += 2
-                    end
-                    g = gnew(;type=argv[2], args...)
-                    show(stdout, "text/plain", g)
-                    println()
-                    if v
-                        g_dot = graph2dot(g, type=type, mode=args[:mode])
-                        kroki(g_dot, type="graphviz")
-                    end
-                    if o
-                        nrows = names(g,1)
-                        ncols = names(g,2)
-                        mat = g.array
-                        header = reshape(vcat("Nodes",ncols),1,:)
-                        content = hcat(nrows,mat)
-                        final = vcat(header,content)
-                        open(output, "w") do io
-                            println(io, get(args, :mode, "directed"))
-                            println(io, argv[2])
-                            writedlm(io,final,",")
-                        end
-                    end
-                end
-            end
-            
-        elseif argv[1] == "--create-data"
-            if !("--graph" in argv)
-                error("A .csv containing an adjacency matrix should be given using --graph.")
-            end
-            args = Dict{Symbol,Any}()
-            valid_args = ["--steps", "--iterations", "--graph", "-o"]
-            o = false
-            output = "create-data-output.csv"
-            filename = nothing
-            i = 2
-            while i <= length(argv)
-                val = nothing
-                if !(argv[i] in valid_args)
-                    error("Option $(argv[i]) not valid for --create-data. See valid options: $valid_args.\n")
-                end
-
-                if (i+1) <= length(argv) && !(argv[i+1] in valid_args)
-                    val = argv[i+1]
-                end
-
-                if argv[i] == "--steps"
-                    valint = tryparse(Int, val)
-                    if isnothing(valint)
-                        error("The value for --steps must be an integer. You provided: $val.")
-                    end
-                    args[:n] = valint
-                elseif argv[i] == "--iterations"
-                    valint = tryparse(Int, val)
-                    if isnothing(valint)
-                        error("The value for --iterations must be an integer. You provided: $val.")
-                    end
-                    args[:iter] = valint
-                elseif argv[i] == "--graph" 
-                    if isnothing(val)
-                        error("Argument --graph requires a filename.")
-                    end
-                    if !endswith(val, ".csv")
-                        error("Input file must be a .csv file. You provided: $val")
-                    end
-                    filename = abspath(val)
-                    if !isfile(filename)
-                        error("File does not exist at: $filename.")
-                    end
-                elseif argv[i] == "-o"
-                    o = true
-                    if isnothing(val)
-                        i += 1
-                        continue
-                    else
-                        output = val
-                    end
-                end
-                i += 2
-            end
-            f = open(filename,"r")
-            graph_mode = strip(readline(f))
-            type = strip(readline(f))
-            close(f)
-            data, header = readdlm(filename,',', header=true, skipstart=2)
-            cnames = vec(header[1,2:end])
-            data = Int64.(data[:,2:end])
-            g = NamedArray(data,(cnames,cnames))
-            g_dat = graph2data(g; args...)
-            if o
-                open(output, "w") do io
-                    final = vcat(permutedims(names(g_dat)), Matrix(g_dat))
-                    println(io, graph_mode)
-                    println(io, type)
-                    writedlm(io,final,",")
-                end
-            else
-                show(stdout, "text/plain", g_dat)
+        type = argv[2]
+        
+        args = Dict{Symbol,Any}()
+        valid_args = ["--nodes", "--edges", "-v", "--mode", "-o"]
+        v = false
+        o = false
+        output = "gnew-output.csv"
+        i = 3
+        while i <= length(argv)
+            val = nothing
+            if !(argv[i] in valid_args)
+                error("Option $(argv[i]) not valid for --gnew. See valid options: $valid_args.\n")
             end
 
-        elseif argv[1] == "--snha"
-            if !("--data" in argv)
-                error("A .csv containing an adjacency matrix should be given using --data.")
+            if (i+1) <= length(argv) && !(argv[i+1] in valid_args)
+                val = argv[i+1]
             end
-            args = Dict{Symbol,Any}()
-            valid_args = ["--data", "--method", "--alpha", "-v", "--threshold"]
-            v = false
-            filename = nothing
-            i = 2
-            while i <= length(argv)
-                val = nothing
-                if !(argv[i] in valid_args)
-                    error("Option $(argv[i]) not valid for --snha. See valid options: $valid_args.\n")
-                end
 
-                if (i+1) <= length(argv) && !(argv[i+1] in valid_args)
-                    val = argv[i+1]
+            if argv[i] == "--nodes"
+                valint = tryparse(Int, val)
+                if isnothing(valint)
+                    error("The value for --nodes must be an integer. You provided: $val.")
                 end
-
-                if argv[i] == "--alpha"
-                    valfl = tryparse(Float64, val)
-                    if isnothing(valfl) || valfl > 1.0 || valfl < 0.0
-                        error("The value for --alpha must be an float between 0.00 and 1.00. You provided: $val.")
-                    end
-                    args[:alpha] = valfl
-                elseif argv[i] == "--threshold"
-                    valfl = tryparse(Float64, val)
-                    if isnothing(valfl) || valfl > 1.0 || valfl < 0.0
-                        error("The value for --threshold must be an float between 0.00 and 1.00. You provided: $val.")
-                    end
-                    args[:threshold] = valfl
-                elseif argv[i] == "--data" 
-                    if isnothing(val)
-                        error("Argument --data requires a filename.")
-                    end
-                    if !endswith(val, ".csv")
-                        error("Input file must be a .csv file. You provided: $val")
-                    end
-                    filename = abspath(val)
-                    if !isfile(filename)
-                        error("File does not exist at: $filename.")
-                    end
-                elseif argv[i] == "-v"
-                    v = true
+                args[:nodes] = valint
+            elseif argv[i] == "--edges"
+                valint = tryparse(Int, val)
+                if isnothing(valint)
+                    error("The value for --edges must be an integer. You provided: $val.")
+                end
+                args[:edges] = valint
+            elseif argv[i] == "-v" 
+                v = true
+                i += 1
+                continue
+            elseif argv[i] == "--mode"
+                if !(val in ["directed", "undirected"])
+                    error("The value for --mode should be 'directed' or 'undirected'. You provided: $val.")
+                end
+                args[:mode] = val
+            elseif argv[i] == "-o"
+                o = true
+                if isnothing(val)
                     i += 1
+                    continue
+                else
+                    output = val
                 end
-                i += 2
             end
-            f = open(filename,"r")
-            graph_mode = strip(readline(f))
-            type = strip(readline(f))
-            close(f)
-            data, header = readdlm(filename,',', header=true, skipstart=2)
-            df = DataFrame(data, Symbol.(vec(header[1,1:end])))
-            g_snha = asg(df; args...)
-            
-            if v
-                g_dot = graph2dot(g_snha["theta"], type=type, mode=graph_mode)
-                kroki(g_dot, type="graphviz")
-            else
-                show(stdout, "text/plain", g_snha["theta"])
-                println()
+            i += 2
+        end
+        g = gnew(;type=type, args...)
+        show(stdout, "text/plain", g)
+        println()
+        println(v)
+        if v
+            g_dot = graph2dot(g, type=type, mode=args[:mode])
+            kroki(g_dot, type="graphviz", cache = false, name="gnew-graph")
+        end
+        if o
+            nrows = names(g,1)
+            ncols = names(g,2)
+            mat = g.array
+            header = reshape(vcat("Nodes",ncols),1,:)
+            content = hcat(nrows,mat)
+            final = vcat(header,content)
+            open(output, "w") do io
+                println(io, get(args, :mode, "directed"))
+                println(io, type)
+                writedlm(io,final,",")
             end
+        end
+        
+    elseif argv[1] == "--create-data"
+        if !("--graph" in argv)
+            error("A .csv containing an adjacency matrix should be given using --graph.")
+        end
+        args = Dict{Symbol,Any}()
+        valid_args = ["--steps", "--iterations", "--graph", "-o"]
+        o = false
+        output = "create-data-output.csv"
+        filename = nothing
+        i = 2
+        while i <= length(argv)
+            val = nothing
+            if !(argv[i] in valid_args)
+                error("Option $(argv[i]) not valid for --create-data. See valid options: $valid_args.\n")
+            end
+
+            if (i+1) <= length(argv) && !(argv[i+1] in valid_args)
+                val = argv[i+1]
+            end
+
+            if argv[i] == "--steps"
+                valint = tryparse(Int, val)
+                if isnothing(valint)
+                    error("The value for --steps must be an integer. You provided: $val.")
+                end
+                args[:n] = valint
+            elseif argv[i] == "--iterations"
+                valint = tryparse(Int, val)
+                if isnothing(valint)
+                    error("The value for --iterations must be an integer. You provided: $val.")
+                end
+                args[:iter] = valint
+            elseif argv[i] == "--graph" 
+                if isnothing(val)
+                    error("Argument --graph requires a filename.")
+                end
+                if !endswith(val, ".csv")
+                    error("Input file must be a .csv file. You provided: $val")
+                end
+                filename = abspath(val)
+                if !isfile(filename)
+                    error("File does not exist at: $filename.")
+                end
+            elseif argv[i] == "-o"
+                o = true
+                if isnothing(val)
+                    i += 1
+                    continue
+                else
+                    output = val
+                end
+            end
+            i += 2
+        end
+        f = open(filename,"r")
+        graph_mode = strip(readline(f))
+        type = strip(readline(f))
+        close(f)
+        data, header = readdlm(filename,',', header=true, skipstart=2)
+        cnames = vec(header[1,2:end])
+        data = Int64.(data[:,2:end])
+        g = NamedArray(data,(cnames,cnames))
+        g_dat = graph2data(g; args...)
+        if o
+            open(output, "w") do io
+                final = vcat(permutedims(names(g_dat)), Matrix(g_dat))
+                println(io, graph_mode)
+                println(io, type)
+                writedlm(io,final,",")
+            end
+        else
+            show(stdout, "text/plain", g_dat)
+        end
+
+    elseif argv[1] == "--snha"
+        if !("--data" in argv)
+            error("A .csv containing an adjacency matrix should be given using --data.")
+        end
+        args = Dict{Symbol,Any}()
+        valid_args = ["--data", "--method", "--alpha", "-v", "--threshold"]
+        v = false
+        filename = nothing
+        i = 2
+        while i <= length(argv)
+            val = nothing
+            if !(argv[i] in valid_args)
+                error("Option $(argv[i]) not valid for --snha. See valid options: $valid_args.\n")
+            end
+
+            if (i+1) <= length(argv) && !(argv[i+1] in valid_args)
+                val = argv[i+1]
+            end
+
+            if argv[i] == "--alpha"
+                valfl = tryparse(Float64, val)
+                if isnothing(valfl) || valfl > 1.0 || valfl < 0.0
+                    error("The value for --alpha must be an float between 0.00 and 1.00. You provided: $val.")
+                end
+                args[:alpha] = valfl
+            elseif argv[i] == "--threshold"
+                valfl = tryparse(Float64, val)
+                if isnothing(valfl) || valfl > 1.0 || valfl < 0.0
+                    error("The value for --threshold must be an float between 0.00 and 1.00. You provided: $val.")
+                end
+                args[:threshold] = valfl
+            elseif argv[i] == "--data" 
+                if isnothing(val)
+                    error("Argument --data requires a filename.")
+                end
+                if !endswith(val, ".csv")
+                    error("Input file must be a .csv file. You provided: $val")
+                end
+                filename = abspath(val)
+                if !isfile(filename)
+                    error("File does not exist at: $filename.")
+                end
+            elseif argv[i] == "-v"
+                v = true
+                i += 1
+                continue
+            end
+            i += 2
+        end
+        f = open(filename,"r")
+        graph_mode = strip(readline(f))
+        type = strip(readline(f))
+        close(f)
+        data, header = readdlm(filename,',', header=true, skipstart=2)
+        df = DataFrame(Float64.(data), Symbol.(vec(header[1,1:end])))
+        g_snha = asg(df; args...)
+        
+        if v
+            g_dot = graph2dot(g_snha["theta"], type=type, mode=graph_mode)
+            kroki(g_dot, type="graphviz", cache=false,name="snha-graph")
+        else
+            show(stdout, "text/plain", g_snha["theta"])
+            println()
         end
 
     else
